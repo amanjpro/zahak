d98acf9b6fd4a837578b19877a8f4c290cecd01c
amanjpro <amanjpro@gmail.com>
2021-02-09 21:14:38 -0500

    Fix pgn and add a unit test

--- pgn.go
+++ pgn.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func (p *Position) ParseMoves(moveStr []string) []*Move {
 	if len(moveStr) == 0 {
 		return []*Move{}
 	}
-	var parsed *Move
-	for _, move := range p.LegalMoves() {
-		if move.ToString() == moveStr[0] {
-			parsed = move
-			break
+	currentMove := moveStr[0]
+	if len(strings.TrimSpace(currentMove)) == 0 {
+		return p.ParseMoves(moveStr[1:])
+	} else {
+		var parsed *Move
+		for _, move := range p.LegalMoves() {
+			if move.ToString() == currentMove {
+				parsed = move
+				break
+			}
 		}
+		if parsed == nil {
+			panic(fmt.Sprintf("Expectd a valid move, %s is not valid", currentMove))
+		}
+		tg := p.tag
+		ep := p.enPassant
+		cp := p.MakeMove(parsed)
+		otherMoves := p.ParseMoves(moveStr[1:])
+		p.UnMakeMove(parsed, tg, ep, cp)
+		return append(append([]*Move{}, parsed), otherMoves...)
 	}
-	if parsed == nil {
-		panic(fmt.Sprintf("Expectd a valid move, %s is not valid", moveStr))
-	}
-	tg := p.tag
-	ep := p.enPassant
-	cp := p.MakeMove(parsed)
-	otherMoves := p.ParseMoves(moveStr[1:])
-	p.UnMakeMove(parsed, tg, ep, cp)
-	return append(append([]*Move{}, parsed), otherMoves...)
 }
