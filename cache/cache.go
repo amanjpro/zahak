e3a1bfc6cc7bce48eec108c1b86ffcc9ffc1f6ec
amanjpro <amanjpro@gmail.com>
2021-02-11 20:44:35 -0500

    Simplify cache, to make it faster in general

--- cache/cache.go
+++ cache/cache.go
@@ -6,37 +6,24 @@ type CachedEval struct {
 }
 
 type Cache struct {
-	itemss  [5]map[uint64]*CachedEval
-	current int
+	items map[uint64]*CachedEval
 }
 
 var TranspositionTable Cache
 
-func (c *Cache) Rotate() {
-	nextCurrent := (c.current + 1) % len(c.itemss)
-	c.itemss[nextCurrent] = make(map[uint64]*CachedEval, 1000_000)
-	c.current = nextCurrent
-}
-
 func (c *Cache) Set(key uint64, value *CachedEval) {
-	c.itemss[c.current][key] = value
+	c.items[key] = value
 }
 
 func (c *Cache) Get(key uint64) (*CachedEval, bool) {
-	for i, j := 0, c.current; i <= len(c.itemss); i++ {
-		item, found := c.itemss[j%len(c.itemss)][key]
-		if found {
-			return item, found
-		}
-		j++
+	item, found := c.items[key]
+	if found {
+		return item, found
 	}
 	return nil, false
 }
 
 func ResetCache() {
-	var itemss [5]map[uint64]*CachedEval
-	for i := 0; i < len(itemss); i++ {
-		itemss[i] = make(map[uint64]*CachedEval, 1000_000)
-	}
-	TranspositionTable = Cache{itemss: itemss, current: 0}
+	items := make(map[uint64]*CachedEval, 1000_000)
+	TranspositionTable = Cache{items: items} //s, current: 0}
 }
