054653d1d51bccb6072ebdd50b79b1e8b339ddc6
amanjpro <amanjpro@gmail.com>
2021-02-15 08:30:22 -0500

    Fix quiescence search and add memoeization

--- search/quiescence.go
+++ search/quiescence.go
@@ -1,52 +1,62 @@
 package search
 
 import (
+	. "github.com/amanjpro/zahak/cache"
 	. "github.com/amanjpro/zahak/engine"
 	. "github.com/amanjpro/zahak/evaluation"
 )
 
-func quescence(position *Position, isMaximizingPlayer bool, alpha int, beta int, ply int) int {
-	legalMoves := position.QuiesceneMoves(ply <= 6)
-	orderedMoves := orderMoves(&ValidMoves{position, legalMoves})
+func quiescence(position *Position, alpha int, beta int, ply int) int {
 
-	if position.Status() == Checkmate {
-		if isMaximizingPlayer {
-			return -CHECKMATE_EVAL
-		}
-		return CHECKMATE_EVAL
+	hash := position.Hash()
+
+	item, found := QCache.Get(hash)
+	if found {
+		return item.Eval
 	}
+
+	outcome := position.Status()
+	if outcome == Checkmate {
+		return -CHECKMATE_EVAL
+	} else if outcome == Draw {
+		return 0
+	}
+
+	legalMoves := position.QuiesceneMoves(ply <= 4)
+	orderedMoves := orderMoves(&ValidMoves{position, legalMoves})
 
 	standPat := Evaluate(position)
 	if standPat >= beta {
-		return beta
+		return standPat
 	}
+
+	// Delta pruning
+	// w := WhitePawn
+	// deltaMargin := w.Weight() * 2 // 200 centipawns
+	//
+	// if standPat < alpha-deltaMargin {
+	// 	return alpha
+	// }
+
 	if alpha < standPat {
 		alpha = standPat
 	}
 
 	if STOP_SEARCH_GLOBALLY {
-		if isMaximizingPlayer {
-			return standPat
-		} else {
-			return -standPat
-		}
+		return standPat
 	}
-
-	bestScore := standPat
 
 	for _, move := range orderedMoves {
 		cp, ep, tg := position.MakeMove(move)
-		score := -quescence(position, !isMaximizingPlayer, -beta, -alpha, ply+1)
+		score := -quiescence(position, -beta, -alpha, ply+1)
 		position.UnMakeMove(move, tg, ep, cp)
-		if score > bestScore {
-			bestScore = score
-			if score > alpha {
-				if score >= beta {
-					break
-				}
-				alpha = score
-			}
+		if score >= beta {
+			return beta
+		}
+		if score > alpha {
+			alpha = score
 		}
 	}
-	return bestScore
+	QCache.Set(hash, &QuiescenceEval{hash, alpha})
+	return alpha
 }
