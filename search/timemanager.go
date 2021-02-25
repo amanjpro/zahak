eddd15b4b2829e6883365fc984979f98fa7bcba5
amanjpro <amanjpro@gmail.com>
2021-02-25 08:25:19 -0500

    Make time management slightly better

--- search/timemanager.go
+++ search/timemanager.go
@@ -10,14 +10,17 @@ import (
 func (e *Engine) InitiateTimer(game *Game, availableTimeInMillis int, isPerMove bool,
 	increment int, movesToTimeControl int, done *sync.WaitGroup, stopTimer chan bool) {
 	maximumTimeToThink := 0
+	numberOfMovesOutOfBook := int(game.MoveClock()) // FIXME: Yup, fix it
+	nMoves := min(numberOfMovesOutOfBook, 10)
+	factor := 2 - nMoves/10
 	if isPerMove {
 		maximumTimeToThink = availableTimeInMillis - 100 + increment
 	} else {
 		if movesToTimeControl == 0 {
-			mlh := 60 // We assume that there are 60 more moves to go
+			mlh := max(60-int(game.MoveClock()), 20) // We assume that there are 60 more moves to go
 			timeInMinute := time.Duration(availableTimeInMillis).Minutes()
 			if timeInMinute <= 15 {
-				mlh = 50 // shorter games have shorter moves, hopefully
+				mlh = max(50-int(game.MoveClock()), 20) // We assume that there are 60 more moves to go
 			}
 			if game.Position().IsEndGame() {
 				movesToTimeControl = abs(mlh)
@@ -25,11 +28,9 @@ func (e *Engine) InitiateTimer(game *Game, availableTimeInMillis int, isPerMove 
 				movesToTimeControl = abs(mlh + 10) // add 10 more moves in the early stage
 			}
 		}
-		if game.Position().IsEndGame() {
-			maximumTimeToThink = max(availableTimeInMillis/movesToTimeControl, 1000) // Naiive, but works for now
-		} else {
-			maximumTimeToThink = max(availableTimeInMillis/movesToTimeControl, 1000) // Naiive, but works for now
-		}
+
+		target := availableTimeInMillis / movesToTimeControl
+		maximumTimeToThink = factor * target
 	}
 
 	select {
@@ -51,6 +52,13 @@ func abs(num int) int {
 
 func max(a int, b int) int {
 	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a int, b int) int {
+	if a < b {
 		return a
 	}
 	return b
