c7af65e60194c2b01d5320fd9aaae5fbad3db92d
amanjpro <amanjpro@gmail.com>
2021-03-06 09:20:02 -0500

    Tweaks in Timemanager

--- search/timemanager.go
+++ search/timemanager.go
@@ -1,31 +1,22 @@
 package search
 
 import (
-	"time"
-
 	. "github.com/amanjpro/zahak/engine"
 )
 
 func (e *Engine) InitiateTimer(game *Game, availableTimeInMillis int, isPerMove bool,
 	increment int, movesToTimeControl int) {
 	maximumTimeToThink := 0
-	numberOfMovesOutOfBook := int(game.MoveClock()) / 2 // FIXME: Yup, fix it
+	numberOfMovesOutOfBook := int(game.MoveClock()) // / 2 // FIXME: Yup, fix it
 	nMoves := min(numberOfMovesOutOfBook, 10)
+	availableTimeInMillis += increment
 	factor := 2 - nMoves/10
 	if isPerMove {
-		maximumTimeToThink = availableTimeInMillis - 100 + increment
+		maximumTimeToThink = availableTimeInMillis - 100
 	} else {
 		if movesToTimeControl == 0 {
-			mlh := max(60-int(game.MoveClock()), 20) // We assume that there are 60 more moves to go
-			timeInMinute := time.Duration(availableTimeInMillis).Minutes()
-			if timeInMinute <= 15 {
-				mlh = max(50-int(game.MoveClock()), 20) // We assume that there are 60 more moves to go
-			}
-			if game.Position().IsEndGame() {
-				movesToTimeControl = abs(mlh)
-			} else {
-				movesToTimeControl = abs(mlh + 10) // add 10 more moves in the early stage
-			}
+			mlh := max(50-int(game.MoveClock()), 20) // We assume that there are 40 moves to go
+			movesToTimeControl = mlh
 		}
 
 		target := availableTimeInMillis / movesToTimeControl
