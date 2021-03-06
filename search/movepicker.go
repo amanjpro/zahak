a5c419b310010b01e8df7e2fff506d83eab060ed
amanjpro <amanjpro@gmail.com>
2021-03-06 09:17:15 -0500

    Tweak moveordering scores

--- search/movepicker.go
+++ search/movepicker.go
@@ -91,14 +91,14 @@ func (mp *MovePicker) score() {
 				// SEE for ordering
 				gain := board.StaticExchangeEval(move.Destination, capPiece, move.Source, piece)
 				if gain < 0 {
-					mp.scores[i] = -100_000_000 + gain
+					mp.scores[i] = -90_000_000 + gain
 				} else if gain == 0 {
 					mp.scores[i] = 100_000_000 + capPiece.Weight() - piece.Weight()
 				} else {
-					mp.scores[i] = 100_010_000 + gain
+					mp.scores[i] = 100_100_000 + gain
 				}
 			} else {
-				mp.scores[i] = 100_000_000 + capPiece.Weight() - piece.Weight()
+				mp.scores[i] = 100_100_000 + capPiece.Weight() - piece.Weight()
 			}
 			continue
 		}
