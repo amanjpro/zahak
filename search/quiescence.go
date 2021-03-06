31bcb6f9bd51313a42c69532a35e2e0db0d53f89
amanjpro <amanjpro@gmail.com>
2021-03-06 09:18:17 -0500

    Cleanup delta-pruning

--- search/quiescence.go
+++ search/quiescence.go
@@ -69,58 +69,9 @@ func (e *Engine) quiescence(position *Position, alpha int32, beta int32, current
 			}
 		}
 
-		// Futility Pruning
-		isCheckMove := move.HasTag(Check)
-		if isInCheck && futility <= alpha && !isCheckMove && move.PromoType == NoType {
-			movingPiece := position.Board.PieceAt(move.Source)
-			futility += movingPiece.Weight()
-			promoting := (movingPiece == BlackPawn && move.Source.Rank() <= Rank5) ||
-				movingPiece == WhitePawn && move.Source.Rank() >= Rank3
-			if !promoting && futility <= alpha {
-				continue
-			}
-		}
-
 		cp, ep, tg, hc := position.MakeMove(move)
 		sp := Evaluate(position)
 
-		var score int32
-		callQuiescence := true
-		if !isInCheck && !isCheckMove {
-			// The logic looks difficult, but it is not
-			// I basically pretend that I have called quiescence
-			// with the reversed alpha/beta (like normal in negamax)
-			// and then, if the bounds exceeded I pretend that I return
-			// either alpha or beta
-			newAlpha := -beta
-			newBeta := -alpha
-			q := WhiteQueen
-			deltaMargin := q.Weight()
-			if move.PromoType != NoType {
-				promo := GetPiece(move.PromoType, White)
-				deltaMargin += promo.Weight()
-			}
-			if sp >= newBeta {
-				e.info.deltaPruningCounter += 1
-				score = -newBeta
-				position.UnMakeMove(move, tg, ep, cp, hc)
-				callQuiescence = false
-			}
-			if sp+deltaMargin < newAlpha { // is capture
-				e.info.deltaPruningCounter += 1
-				position.UnMakeMove(move, tg, ep, cp, hc)
-				callQuiescence = false
-				score = -newAlpha
-			}
-		}
-
-		if callQuiescence {
-			v, ok := e.quiescence(position, -beta, -alpha, move, ply+1, sp, searchHeight+1)
-			position.UnMakeMove(move, tg, ep, cp, hc)
-			if !ok {
-				return v, ok
-			}
-			score = -v
 		e.pred.Push(position.Hash())
 		v, ok := e.quiescence(position, -beta, -alpha, move, ply+1, sp, searchHeight+1)
 		e.pred.Pop()
