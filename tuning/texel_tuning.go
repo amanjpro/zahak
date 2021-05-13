2f69a25262a84f87d48e1e59a52db62c2b30e3b2
amanjpro <amanjpro@gmail.com>
2021-05-13 09:00:39 -0400

    Fix local-optimize and make it work

--- tuning/texel_tuning.go
+++ tuning/texel_tuning.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
-	"strconv"
+	// "strconv"
 	"strings"
 	// "sync"
 
@@ -27,7 +28,6 @@ var initialK = 1.0
 var answers = make(chan float64)
 var ml = NewMoveList(500)
 var MAXEPOCHS = 10000
-var BATCHSIZE int
 var REPORTING = 50
 var LRSTEPRATE = 250
 var LRATE = 1.00
@@ -74,12 +74,12 @@ func computeInitialGuesses() []int16 {
 	guesses = append(guesses, LateQueenPst[:]...)                  // 576-639
 	guesses = append(guesses, EarlyKingPst[:]...)                  // 640-703
 	guesses = append(guesses, LateKingPst[:]...)                   // 704-767
-	guesses = append(guesses, MiddlegameBackwardPawnAward)         // 768
-	guesses = append(guesses, EndgameBackwardPawnAward)            // 769
-	guesses = append(guesses, MiddlegameIsolatedPawnAward)         // 770
-	guesses = append(guesses, EndgameIsolatedPawnAward)            // 771
-	guesses = append(guesses, MiddlegameDoublePawnAward)           // 772
-	guesses = append(guesses, EndgameDoublePawnAward)              // 773
+	guesses = append(guesses, MiddlegameBackwardPawnPenalty)       // 768
+	guesses = append(guesses, EndgameBackwardPawnPenalty)          // 769
+	guesses = append(guesses, MiddlegameIsolatedPawnPenalty)       // 770
+	guesses = append(guesses, EndgameIsolatedPawnPenalty)          // 771
+	guesses = append(guesses, MiddlegameDoublePawnPenalty)         // 772
+	guesses = append(guesses, EndgameDoublePawnPenalty)            // 773
 	guesses = append(guesses, MiddlegamePassedPawnAward)           // 774
 	guesses = append(guesses, EndgamePassedPawnAward)              // 775
 	guesses = append(guesses, MiddlegameCandidatePassedPawnAward)  // 776
@@ -94,9 +94,11 @@ func computeInitialGuesses() []int16 {
 	guesses = append(guesses, EndgameHorizontalDoubleRookAward)    // 785
 	guesses = append(guesses, MiddlegamePawnFactorCoeff)           // 786
 	guesses = append(guesses, EndgamePawnFactorCoeff)              // 787
-	guesses = append(guesses, MiddlegameAggressivityFactorCoeff)   // 788
-	guesses = append(guesses, EndgameAggressivityFactorCoeff)      // 789
-	guesses = append(guesses, MiddlegameCastlingAward)             // 790
+	guesses = append(guesses, MiddlegameMobilityFactorCoeff)       // 788
+	guesses = append(guesses, EndgameMobilityFactorCoeff)          // 789
+	guesses = append(guesses, MiddlegameAggressivityFactorCoeff)   // 790
+	guesses = append(guesses, EndgameAggressivityFactorCoeff)      // 791
+	// guesses = append(guesses, MiddlegameCastlingAward)             // 792
 	return guesses
 }
 
@@ -115,12 +117,12 @@ func updateEvalParams(guesses []int16) {
 		EarlyKingPst[i] = guesses[i+10*64]
 		LateKingPst[i] = guesses[i+11*64]
 	}
-	MiddlegameBackwardPawnAward = guesses[768]
-	EndgameBackwardPawnAward = guesses[769]
-	MiddlegameIsolatedPawnAward = guesses[770]
-	EndgameIsolatedPawnAward = guesses[771]
-	MiddlegameDoublePawnAward = guesses[772]
-	EndgameDoublePawnAward = guesses[773]
+	MiddlegameBackwardPawnPenalty = guesses[768]
+	EndgameBackwardPawnPenalty = guesses[769]
+	MiddlegameIsolatedPawnPenalty = guesses[770]
+	EndgameIsolatedPawnPenalty = guesses[771]
+	MiddlegameDoublePawnPenalty = guesses[772]
+	EndgameDoublePawnPenalty = guesses[773]
 	MiddlegamePassedPawnAward = guesses[774]
 	EndgamePassedPawnAward = guesses[775]
 	MiddlegameCandidatePassedPawnAward = guesses[776]
@@ -135,9 +137,11 @@ func updateEvalParams(guesses []int16) {
 	EndgameHorizontalDoubleRookAward = guesses[785]
 	MiddlegamePawnFactorCoeff = guesses[786]
 	EndgamePawnFactorCoeff = guesses[787]
-	MiddlegameAggressivityFactorCoeff = guesses[788]
-	EndgameAggressivityFactorCoeff = guesses[789]
-	MiddlegameCastlingAward = guesses[790]
+	MiddlegameMobilityFactorCoeff = guesses[788]
+	EndgameMobilityFactorCoeff = guesses[789]
+	MiddlegameAggressivityFactorCoeff = guesses[790]
+	EndgameAggressivityFactorCoeff = guesses[791]
+	// MiddlegameCastlingAward = guesses[792]
 }
 
 func toEvalParams(guesses []float64) []int16 {
@@ -175,12 +179,12 @@ func printOptimalGuesses(guesses []int16) {
 	printPst(guesses[576:640], "LateQueenPst")
 	printPst(guesses[704:768], "LateKingPst")
 
-	fmt.Printf("var MiddlegameBackwardPawnAward int16 = %d\n", guesses[768])
-	fmt.Printf("var EndgameBackwardPawnAward int16 = %d\n", guesses[769])
-	fmt.Printf("var MiddlegameIsolatedPawnAward int16 = %d\n", guesses[770])
-	fmt.Printf("var EndgameIsolatedPawnAward int16 = %d\n", guesses[771])
-	fmt.Printf("var MiddlegameDoublePawnAward int16 = %d\n", guesses[772])
-	fmt.Printf("var EndgameDoublePawnAward int16 = %d\n", guesses[773])
+	fmt.Printf("var MiddlegameBackwardPawnPenalty int16 = %d\n", guesses[768])
+	fmt.Printf("var EndgameBackwardPawnPenalty int16 = %d\n", guesses[769])
+	fmt.Printf("var MiddlegameIsolatedPawnPenalty int16 = %d\n", guesses[770])
+	fmt.Printf("var EndgameIsolatedPawnPenalty int16 = %d\n", guesses[771])
+	fmt.Printf("var MiddlegameDoublePawnPenalty int16 = %d\n", guesses[772])
+	fmt.Printf("var EndgameDoublePawnPenalty int16 = %d\n", guesses[773])
 	fmt.Printf("var MiddlegamePassedPawnAward int16 = %d\n", guesses[774])
 	fmt.Printf("var EndgamePassedPawnAward int16 = %d\n", guesses[775])
 	fmt.Printf("var MiddlegameCandidatePassedPawnAward int16 = %d\n", guesses[776])
@@ -195,28 +199,30 @@ func printOptimalGuesses(guesses []int16) {
 	fmt.Printf("var EndgameHorizontalDoubleRookAward int16 = %d\n", guesses[785])
 	fmt.Printf("var MiddlegamePawnFactorCoeff int16 = %d\n", guesses[786])
 	fmt.Printf("var EndgamePawnFactorCoeff int16 = %d\n", guesses[787])
-	fmt.Printf("var MiddlegameAggressivityFactorCoeff int16 = %d\n", guesses[788])
-	fmt.Printf("var EndgameAggressivityFactorCoeff int16 = %d\n", guesses[789])
-	fmt.Printf("var MiddlegameCastlingAward int16 = %d\n", guesses[790])
+	fmt.Printf("var MiddlegameMobilityFactorCoeff int16 = %d\n", guesses[788])
+	fmt.Printf("var EndgameMobilityFactorCoeff int16 = %d\n", guesses[789])
+	fmt.Printf("var MiddlegameAggressivityFactorCoeff int16 = %d\n", guesses[790])
+	fmt.Printf("var EndgameAggressivityFactorCoeff int16 = %d\n", guesses[791])
+	// fmt.Printf("var MiddlegameCastlingAward int16 = %d\n", guesses[792])
 	fmt.Println("===================================================")
 }
 
 func localOptimize(initialGuess []int16, K float64) []int16 {
 	nParams := len(initialGuess)
-	bestE := E(initialGuess, K)
+	bestE := meanSquareError(testPositions, initialGuess, K)
 	bestParValues := append([]int16{}, initialGuess...)
 	improved := true
 	for improved {
 		improved = false
 		for pi := 0; pi < nParams; pi++ {
-			// if pi < 768 {
-			// 	continue
-			// }
+			if pi < 788 {
+				continue
+			}
 			if _, ok := futileIndices[pi]; ok {
 				continue
 			}
 			bestParValues[pi] += 1
-			newE := E(bestParValues, K)
+			newE := meanSquareError(testPositions, bestParValues, K)
 			if newE < bestE {
 				bestE = newE
 				fmt.Println("Best parameters so far")
@@ -224,7 +230,11 @@ func localOptimize(initialGuess []int16, K float64) []int16 {
 				improved = true
 			} else {
 				bestParValues[pi] -= 2
-				newE = E(bestParValues, K)
+				if pi >= 768 && bestParValues[pi] < 0 {
+					bestParValues[pi] += 1
+					continue
+				}
+				newE = meanSquareError(testPositions, bestParValues, K)
 				if newE < bestE {
 					bestE = newE
 					fmt.Println("Best parameters so far")
@@ -234,6 +244,7 @@ func localOptimize(initialGuess []int16, K float64) []int16 {
 					bestParValues[pi] += 1 // reset the guess
 				}
 			}
+			fmt.Println("Current best E", bestE)
 		}
 	}
 	return bestParValues
@@ -245,7 +256,7 @@ func findK() float64 {
 	step := 1.0
 	curr := start
 	var err float64
-	best := E(initialGuesses, start)
+	best := meanSquareError(testPositions, initialGuesses, start)
 
 	for i := 0; i < K_PRECISION; i++ {
 
@@ -253,7 +264,7 @@ func findK() float64 {
 		curr = start - step
 		for curr < end {
 			curr = curr + step
-			err = E(initialGuesses, curr)
+			err = meanSquareError(testPositions, initialGuesses, curr)
 			if err <= best {
 				best = err
 				start = curr
@@ -274,44 +285,44 @@ func findK() float64 {
 func linearEvaluation(pos *Position) int16 {
 	eval := Evaluate(pos)
 	if pos.Turn() == Black {
-		eval = -eval
+		return -eval
 	}
 	return eval
 }
 
-func processor(start int, end int, K float64) {
+func processor(testPositions []TestPosition, start int, end int, K float64) {
 	var acc float64 = 0
 	for i := start; i < end; i++ {
 		eval := linearEvaluation(testPositions[i].pos)
-		acc = math.Pow(testPositions[i].outcome-sigmoid(eval, K), 2)
+		acc += math.Pow(testPositions[i].outcome-sigmoid(eval, K), 2)
 	}
 
 	answers <- acc
 }
 
-func E(guess []int16, K float64) float64 {
+func meanSquareError(testPositions []TestPosition, guess []int16, K float64) float64 {
 	acc := float64(0)
 	updateEvalParams(guess)
 
+	batchSize := len(testPositions) / NUM_PROCESSORS
 	for i := 0; i < NUM_PROCESSORS; i++ {
-		start := i * BATCHSIZE
-		end := (i + 1) * BATCHSIZE
+		start := i * batchSize
+		end := (i + 1) * batchSize
 		if i == NUM_PROCESSORS-1 {
 			end = len(testPositions)
 		}
-		go processor(start, end, K)
+		go processor(testPositions, start, end, K)
 	}
 
 	for i := 0; i < NUM_PROCESSORS; i++ {
 		ans := <-answers
 		acc += ans
 	}
-	return (1 / float64(len(testPositions))) * acc
+	return acc / float64(len(testPositions))
 }
 
-func sigmoid(E int16, K float64) float64 {
-	exp := K * float64(E) / 400
-	return 1 / (1 + math.Pow(10, -exp))
+func sigmoid(eval int16, K float64) float64 {
+	return (1.0 / (1.0 + math.Pow(10, -(K*float64(eval))/400.0)))
 }
 
 func loadPositions(path string, actionFn func(string)) {
@@ -339,33 +350,33 @@ func loadPositions(path string, actionFn func(string)) {
 
 func parseLine(line string) (string, float64) {
 	fields := strings.Fields(line)
-	// fen := strings.Join(fields[:4], " ")
-	// fen = fmt.Sprintf("%s 0 1", fen)
+	fen := strings.Join(fields[:4], " ")
+	fen = fmt.Sprintf("%s 0 1", fen)
+
+	outcomeStr := strings.Trim(fields[5], "\";")
+	var outcome float64
+	if outcomeStr == "1/2-1/2" {
+		outcome = 0.5
+	} else if outcomeStr == "1-0" {
+		outcome = 1.0
+	} else if outcomeStr == "0-1" {
+		outcome = 0.0
+	} else {
+		panic(fmt.Sprintf("Unexpected output %s", outcomeStr))
+	}
 	//
-	// outcomeStr := strings.Trim(fields[5], "\";")
-	// var outcome float64
-	// if outcomeStr == "1/2-1/2" {
-	// 	outcome = 0.5
-	// } else if outcomeStr == "1-0" {
-	// 	outcome = 1.0
-	// } else if outcomeStr == "0-1" {
-	// 	outcome = 0.0
-	// } else {
-	// 	panic(fmt.Sprintf("Unexpected output %s", outcomeStr))
+	// fen := strings.Trim(strings.Join(fields[:6], " "), ";")
+	//
+	// outcomeStr := strings.Replace(fields[8], "pgn=", "", -1)
+	// outcome, e := strconv.ParseFloat(outcomeStr, 64)
+	// if e != nil {
+	// 	panic(e)
 	// }
-
-	fen := strings.Trim(strings.Join(fields[:6], " "), ";")
-
-	outcomeStr := strings.Replace(fields[8], "pgn=", "", -1)
-	outcome, e := strconv.ParseFloat(outcomeStr, 64)
-	if e != nil {
-		panic(e)
-	}
-	if fields[1] == "b" && outcomeStr == "1.0" {
-		outcome = 0
-	} else if fields[1] == "b" && outcomeStr == "0.0" {
-		outcome = 1
-	}
+	// if fields[1] == "b" && outcomeStr == "1.0" {
+	// 	outcome = 0
+	// } else if fields[1] == "b" && outcomeStr == "0.0" {
+	// 	outcome = 1
+	// }
 
 	return fen, outcome
 
@@ -389,73 +400,6 @@ func PrepareTuningData(path string) {
 	})
 }
 
-func updateSingleGradient(testPosition TestPosition, gradient *[]float64, params []float64, K float64) {
-
-	updateEvalParams(toEvalParams(params))
-	E := linearEvaluation(testPosition.pos)
-	S := sigmoid(E, K)
-	X := (testPosition.outcome - S) * S * (1 - S)
-
-	for i := 0; i < len(params); i++ {
-		(*gradient)[i] += X
-	}
-}
-
-func computeGradient(gradient *[]float64, params []float64, K float64, batch int) {
-
-	local := make([]float64, len(initialGuesses))
-	start := batch * BATCHSIZE
-	end := (batch + 1) * BATCHSIZE
-	if batch == NUM_PROCESSORS-1 {
-		end = len(testPositions)
-	}
-	start = 0
-	for i := start; i < end; i++ {
-		entry := testPositions[i]
-		updateSingleGradient(entry, &local, params, K)
-	}
-
-	for i := 0; i < len(initialGuesses); i++ {
-		(*gradient)[i] += local[i]
-	}
-}
-
-func gradientOptimize(K float64) {
-	params := make([]float64, len(initialGuesses))
-	adagrad := make([]float64, len(initialGuesses))
-	rate := LRATE
-	for epoch := 1; epoch <= MAXEPOCHS; epoch++ {
-		// var waitGroup sync.WaitGroup
-		// waitGroup.Add(NUM_PROCESSORS)
-		for batch := 0; batch < NUM_PROCESSORS; batch++ {
-			gradient := make([]float64, len(initialGuesses))
-			// go func(batch int) {
-			// 	defer waitGroup.Done()
-			computeGradient(&gradient, params, K, batch)
-			for i := 0; i < len(initialGuesses); i++ {
-				adagrad[i] += math.Pow((K/200.0)*gradient[i]/float64(BATCHSIZE), 2.0)
-				adagrad[i] += math.Pow((K/200.0)*gradient[i]/float64(BATCHSIZE), 2.0)
-				params[i] += (K / 200.0) * (gradient[i] / float64(BATCHSIZE)) * (rate / math.Sqrt(1e-8+adagrad[i]))
-				params[i] += (K / 200.0) * (gradient[i] / float64(BATCHSIZE)) * (rate / math.Sqrt(1e-8+adagrad[i]))
-			}
-			//
-			// }(batch)
-		}
-
-		// waitGroup.Wait()
-
-		err := E(toEvalParams(params), K)
-		fmt.Printf("\rEpoch [%d] Error = [%.8f], Rate = [%g]\n", epoch, err, rate)
-		// Pre-scheduled Learning Rate drops
-		if epoch%LRSTEPRATE == 0 {
-			rate = rate / LRDROPRATE
-		}
-		if epoch%REPORTING == 0 {
-			printOptimalGuesses(toEvalParams(params))
-		}
-	}
-}
-
 func Tune(path string) {
 	loadPositions(path, func(line string) {
 		fen, outcome := parseLine(line)
@@ -465,14 +409,198 @@ func Tune(path string) {
 		testPositions = append(testPositions, tp)
 	})
 
-	BATCHSIZE = len(testPositions) / NUM_PROCESSORS
 	fmt.Printf("%d positions loaded\n", len(testPositions))
 	K := findK()
 	fmt.Printf("Optimal K is %f\n", K)
 	optimalGuesses := localOptimize(initialGuesses, K)
-	// gradientOptimize(K)
+	// tuningVars := make([]Parameter, len(initialGuesses))
+	// for i, v := range initialGuesses {
+	// 	tuningVars[i] = NewParameter(v)
+	// 	if _, ok := futileIndices[i]; ok {
+	// 		tuningVars[i].MaxValue = 0
+	// 		tuningVars[i].MinValue = 0
+	// 	} else if i >= 768 {
+	// 		tuningVars[i].MinValue = 0
+	// 	}
+	// }
+	// optimalGuesses := spsaTuning(tuningVars, 1000, K)
 	fmt.Println("Optimal Parameters have been found!!")
 	fmt.Println("===================================================")
 	printOptimalGuesses(optimalGuesses)
 	close(answers)
+}
+
+type Parameter struct {
+	MaxValue float64
+	MinValue float64
+
+	OriginalValue int16
+
+	C_END float64
+	R_END float64
+	C     float64
+	A     float64
+}
+
+func NewParameter(variable int16) Parameter {
+	return Parameter{
+		MaxValue:      300.0,
+		MinValue:      -300.0,
+		OriginalValue: variable,
+		C_END:         4.0,
+		R_END:         0.002,
+		C:             0.0,
+		A:             0.0,
+	}
+}
+
+type DataPoint struct {
+	Iteration int
+
+	ThetaError float64
+
+	ThetaPlusError  float64
+	ThetaMinusError float64
+}
+
+func WriteDataPoint(data []DataPoint) {
+	fmt.Println("Iteration,Error,+Error,-Error")
+	for _, dp := range data {
+		fmt.Printf("%d,%f,%f,%f\n", dp.Iteration, dp.ThetaError, dp.ThetaPlusError, dp.ThetaMinusError)
+	}
+}
+func changedError(newValues []float64, K float64) float64 {
+
+	params := make([]int16, len(newValues))
+	// Step 1. Convert newValues to int16, and send it to meanSquareError (which also updates the current variables)
+	for i := 0; i < len(newValues); i++ {
+		params[i] = int16(newValues[i])
+	}
+
+	// Step 2. Compute the new error and return
+	return meanSquareError(testPositions, params, K)
+}
+
+// This is taken almost verbatim from Loki, many thanks to the author
+// Niels who helped me a lot to undrestand it.
+// https://github.com/BimmerBass/Loki
+// I am currently not using it, because with my current settings, the local optimize did
+// an amazing job, but I intend to come back to this and make it work.
+func spsaTuning(tuningVars []Parameter, iterations int, K float64) []int16 {
+	// Step 2. Set up the vector of parameter values, we call it theta here.
+	data := make([]DataPoint, 0, iterations+1)    // This creates a zero size array (hence the second parameter), but which can grow upto iterations+1
+	theta := make([]float64, len(initialGuesses)) // this creates an array of size len(initializeGuesses)
+	for p := 0; p < len(theta); p++ {
+		theta[p] = float64(tuningVars[p].OriginalValue)
+	}
+
+	// Step 4. Calculate SPSA constants
+
+	BIG_A := 0.1 * float64(iterations)
+	alpha := 0.602
+	gamma := 0.101
+
+	for p := 0; p < len(theta); p++ {
+		tuningVars[p].C = tuningVars[p].C_END * math.Pow(float64(iterations), gamma)
+
+		a_end := tuningVars[p].R_END * math.Pow(tuningVars[p].C, 2.0)
+
+		tuningVars[p].A = a_end * math.Pow(BIG_A+float64(iterations), alpha)
+	}
+
+	// Step 5. Run the tuning with the given number of iterations
+	var theta_plus = make([]float64, len(tuningVars))
+	var theta_minus = make([]float64, len(tuningVars))
+
+	// Initialize perturbation vector.
+	var delta = make([]float64, len(tuningVars))
+
+	/*
+		Zero-initialization of step-size and perturbation vectors
+	*/
+	var an = make([]float64, len(theta))
+	var cn = make([]float64, len(theta))
+
+	data = append(data, DataPoint{Iteration: 0, ThetaError: changedError(theta, K), ThetaPlusError: 0, ThetaMinusError: 0})
+	for n := 0; n < iterations; n++ {
+
+		for p := 0; p < len(tuningVars); p++ {
+			an[p] = tuningVars[p].A / (math.Pow(BIG_A+float64(n)+1, alpha))
+
+			cn[p] = tuningVars[p].C / (math.Pow(float64(n)+1, gamma))
+		}
+
+		// I dont' need to clear theta_plus, theta_minus and data
+		// becase I use delta[p] syntax, instead of push, which overrides
+		// the current values
+
+		// Step 5C. Determine delta and thus theta_plus and theta_minus
+		for p := 0; p < len(tuningVars); p++ {
+			d := randemacher()
+
+			delta[p] = d // Add the scores to the delta vector.
+
+			// Step 5C.1. Compute theta_plus and theta_minus from these values
+
+			theta_plus[p] = math.Round(float64(theta[p]) + float64(d)*cn[p])
+			theta_minus[p] = math.Round(float64(theta[p]) - float64(d)*cn[p])
+		}
+
+		// Step 5D. Compute the error of theta_plus and theta_minus respectively.
+		thetaPlusErr := float64(len(testPositions)) * changedError(theta_plus, K)
+		thetaMinusErr := float64(len(testPositions)) * changedError(theta_minus, K)
+
+		// Step 5E. Compute the gradient for each variable and adjust accordingly.
+		for p := 0; p < len(tuningVars); p++ {
+			g_hat := (thetaPlusErr - thetaMinusErr) / (2.0 * cn[p] * delta[p])
+
+			// Now adjust the theta values based on the gradient.
+			theta[p] -= math.Round(an[p] * g_hat)
+
+			// Lastly, we'll have to make sure that we don't go out of the designated bounds
+			theta[p] = math.Max(tuningVars[p].MinValue, math.Min(tuningVars[p].MaxValue, theta[p]))
+		}
+
+		for p := 0; p < len(tuningVars); p++ {
+			fmt.Printf("Iteration: %d: Tuned: [ %d ]: %f, (Original: [ %d ])\n", n, p, theta[p], tuningVars[p].OriginalValue)
+		}
+
+		// Step 5A. Compute the current error. This is only used for outputting the progress.
+		err := changedError(theta, K)
+		dp := DataPoint{Iteration: n + 1, ThetaError: err, ThetaPlusError: thetaPlusErr, ThetaMinusError: thetaMinusErr}
+		data = append(data, dp)
+		fmt.Printf("Iteration data point: %d,%f,%f,%f\n", dp.Iteration, dp.ThetaError, dp.ThetaPlusError, dp.ThetaMinusError)
+		fmt.Println("----------------------------------")
+	}
+
+	params := make([]int16, len(theta))
+	for i, p := range theta {
+		params[i] = int16(p)
+	}
+
+	WriteDataPoint(data)
+	return params
+}
+
+func min(x, y int16) int16 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int16) int16 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+var rnd = rand.New(rand.NewSource(99))
+
+func randemacher() float64 {
+	if rnd.Intn(2) < 1 {
+		return 1
+	}
+	return -1
 }
