bbdc4958d8c963b38276bbe95d2b6e1f48989da6
amanjpro <amanjpro@gmail.com>
2021-10-08 22:22:59 -0400

    Do not search quiet moves in qs

--- search/quiescence.go
+++ search/quiescence.go
@@ -74,9 +74,12 @@ func (e *Engine) quiescence(alpha int16, beta int16, searchHeight int8) int16 {
 
 	var isInCheck = e.Position.IsInCheck()
 	movePicker := e.MovePickers[searchHeight]
-	movePicker.RecycleWith(position, e, -1, searchHeight, EmptyMove, !isInCheck)
+	movePicker.RecycleWith(position, e, -1, searchHeight, EmptyMove, true) //!isInCheck)
 
-	bestscore := standPat
+	bestscore := -CHECKMATE_EVAL //standPat
+	if !isInCheck {
+		bestscore = standPat
+	}
 	noisyMoves := -1
 	seeScores := movePicker.captureMoveList.Scores
 
