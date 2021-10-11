ab5e54f89d62c121c87824977b97575479ac00e0
amanjpro <amanjpro@gmail.com>
2021-10-11 16:41:32 -0400

    Another attempt

--- search/search.go
+++ search/search.go
@@ -365,21 +365,19 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 
 		// NullMove pruning
 		isNullMoveAllowed := currentMove != EmptyMove && !position.IsEndGame()
-		if isNullMoveAllowed && depthLeft >= 2 && eval > beta {
-			var R = 4 + min8(depthLeft/4, 3) + min8(int8((eval-beta)/256), 3)
+		if isNullMoveAllowed && depthLeft >= 3 && eval > beta {
+			var R = 4 + depthLeft/6 + min8(int8((eval-beta)/256), 2)
 			R = min8(R, depthLeft)
-			if R >= 2 {
-				ep := position.MakeNullMove()
-				e.pred.Push(position.Hash())
-				e.innerLines[searchHeight+1].Recycle()
-				e.positionMoves[searchHeight+1] = EmptyMove
-				score := -e.alphaBeta(depthLeft-R, searchHeight+1, -beta, -beta+1)
-				e.pred.Pop()
-				position.UnMakeNullMove(ep)
-				if score >= beta {
-					e.info.nullMoveCounter += 1
-					return score
-				}
+			ep := position.MakeNullMove()
+			e.pred.Push(position.Hash())
+			e.innerLines[searchHeight+1].Recycle()
+			e.positionMoves[searchHeight+1] = EmptyMove
+			score := -e.alphaBeta(depthLeft-R, searchHeight+1, -beta, -beta+1)
+			e.pred.Pop()
+			position.UnMakeNullMove(ep)
+			if score >= beta {
+				e.info.nullMoveCounter += 1
+				return score
 			}
 		}
 
