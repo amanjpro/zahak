cc2e7b6053372fc24737aa249035d93cebe7e4a5
amanjpro <amanjpro@gmail.com>
2021-10-11 22:57:32 -0400

    Last tweak

--- search/search.go
+++ search/search.go
@@ -373,19 +373,19 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 			// 	R = min8(R, depthLeft-1)
 			// }
 
-			// if R >= 2 {
-			ep := position.MakeNullMove()
-			e.pred.Push(position.Hash())
-			e.innerLines[searchHeight+1].Recycle()
-			e.positionMoves[searchHeight+1] = EmptyMove
-			score := -e.alphaBeta(depthLeft-R, searchHeight+1, -beta, -beta+1)
-			e.pred.Pop()
-			position.UnMakeNullMove(ep)
-			if score >= beta {
-				e.info.nullMoveCounter += 1
-				return score
+			if R >= 2 {
+				ep := position.MakeNullMove()
+				e.pred.Push(position.Hash())
+				e.innerLines[searchHeight+1].Recycle()
+				e.positionMoves[searchHeight+1] = EmptyMove
+				score := -e.alphaBeta(depthLeft-R, searchHeight+1, -beta, -beta+1)
+				e.pred.Pop()
+				position.UnMakeNullMove(ep)
+				if score >= beta {
+					e.info.nullMoveCounter += 1
+					return score
+				}
 			}
-			// }
 		}
 
 		// Prob cut
