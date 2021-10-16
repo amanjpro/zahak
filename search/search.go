f6aafb19a3ea0992a1a460043299893e6f9eb3f1
Amanj Sherwany <sherwany.amanj@gmail.com>
2021-10-16 11:34:04 -0400

    Remove history pruning (#149)
    Remove history pruning

--- search/search.go
+++ search/search.go
@@ -596,8 +596,8 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 		lmrThreashold += 1
 	}
 	seeScores := movePicker.captureMoveList.Scores
-	quietScores := movePicker.quietMoveList.Scores
-	var historyThreashold int32 = int32(depthLeft) * -1024
+	// quietScores := movePicker.quietMoveList.Scores
+	// var historyThreashold int32 = int32(depthLeft) * -1024
 	var move Move
 	for true {
 
@@ -647,13 +647,13 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 				e.info.seeCounter += 1
 				break
 			}
-
-			// History pruning
-			lmrDepth := depthLeft - int8(lmrReductions[min8(31, depthLeft)][min(31, legalMoves+1)])
-			if isQuiet && quietScores[quietMoves] < historyThreashold && lmrDepth < 3 && legalMoves+1 > lmrThreashold {
-				e.info.historyPruningCounter += 1
-				continue
-			}
+			//
+			// // History pruning
+			// lmrDepth := depthLeft - int8(lmrReductions[min8(31, depthLeft)][min(31, legalMoves+1)])
+			// if isQuiet && quietScores[quietMoves] < historyThreashold && lmrDepth < 3 && legalMoves+1 > lmrThreashold {
+			// 	e.info.historyPruningCounter += 1
+			// 	continue
+			// }
 		}
 
 		if oldEnPassant, oldTag, hc, ok := position.MakeMove(move); ok {
