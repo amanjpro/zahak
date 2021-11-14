5238cd678aa1e47050f9f9cf14c47ac6053d7890
amanjpro <amanjpro@gmail.com>
2021-11-14 03:17:21 -0500

    history lmr

--- search/search.go
+++ search/search.go
@@ -682,7 +682,7 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 					LMR += 1
 				}
 
-				LMR -= int8(e.searchHistory.History(position.Turn(), gpMove, currentMove, move) / 24576)
+				// LMR -= int8(e.searchHistory.History(position.Turn(), gpMove, currentMove, move) / 24576)
 
 				LMR = min8(depthLeft-2, max8(LMR, 1))
 			}
