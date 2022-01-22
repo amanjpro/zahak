08da0bbe1139f3c3c6ccbb8338d3e1906a817020
amanjpro <amanjpro@gmail.com>
2022-01-21 21:56:23 -0500

    Tweak RFP

--- search/search.go
+++ search/search.go
@@ -395,11 +395,11 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 		}
 
 		// Reverse Futility Pruning
-		reverseFutilityMargin := int16(depthLeft) * 85 //(b - p)
+		reverseFutilityMargin := int16(depthLeft) * 75 //(b - p)
 		if improving {
-			reverseFutilityMargin -= 85 // int16(depthLeft) * p
+			reverseFutilityMargin -= 75 // int16(depthLeft) * p
 		}
-		if depthLeft < 8 && eval-reverseFutilityMargin >= beta {
+		if depthLeft < 9 && eval-reverseFutilityMargin >= beta {
 			e.info.rfpCounter += 1
 			return eval - reverseFutilityMargin /* fail soft */
 		}
