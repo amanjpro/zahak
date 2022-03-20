3d4b7a0c6b3044c7c23af8a5bf91cf85f1b20725
amanjpro <amanjpro@gmail.com>
2022-03-20 01:15:10 -0400

    Tweaks

--- search/search.go
+++ search/search.go
@@ -336,7 +336,7 @@ func (e *Engine) alphaBeta(depthLeft int8, searchHeight int8, alpha int16, beta 
 
 	e.searchHistory.ResetKillers(searchHeight + 1)
 	// Pruning
-	pruningAllowed := !isRootNode && !isInCheck && e.doPruning && !firstLayerOfSingularity
+	pruningAllowed := (!isPvNode || searchHeight > 4) && !isInCheck && e.doPruning && !firstLayerOfSingularity
 
 	// Idea taken from Berserk
 	histDepth := depthLeft
